6d4deecc5d8f29c6478794b4604c864c02acfebf
bereketsintayehu <bereketsintayehu32@gmail.com>
2024-07-30 02:01:38 -0700

    task 2

---
+++ task-2.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func countWords(str string) map[string]int {
+	count := make(map[string]int)
+	for _, char := range strings.Fields(str) {
+		char = strings.ToLower(char)
+		lst := []string{}
+		for _, c := range char {
+			if (c >= 'a' && c <= 'z') {
+				lst = append(lst, string(c))
+			}
+		}
+		char = strings.Join(lst, "")
+		count[strings.ToLower(char)]++
+	}
+	return count
+}
+
+func isPalindrome(str string) bool {
+	l, r := 0, len(str)-1
+	for l < r {
+		if str[l] != str[r] {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(countWords("a A! A"))
+	fmt.Println(isPalindrome("abba"))
+}
